19841ec72fa154dbd519b8be09852be4591c25e8
ayhanunal <unalayhan@yahoo.com>
2021-06-20 18:52:31 +0300

    ui and button click

--- app/src/main/java/com/ayhanunal/permissionrequest/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/permissionrequest/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ayhanunal.permissionrequest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        permission_request_button.setOnClickListener {
+            //request permission.
+
+        }
+
     }
+
+
 }

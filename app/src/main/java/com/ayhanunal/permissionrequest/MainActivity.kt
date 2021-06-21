4ce31bf47de020d3b24803d5c503caedc2fc5eb0
ayhan ünal <ayhan.unal@edelkrone.com>
2021-06-21 13:28:41 +0300

    permission launcher initialize and permission request

--- app/src/main/java/com/ayhanunal/permissionrequest/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/permissionrequest/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.ayhanunal.permissionrequest
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +24,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        registerLauncher()
 
         permission_request_button.setOnClickListener {
-            //request permission.
+            if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }else{
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
 
+            }else{
+                storage_permission_text.text = "Read External Storage: True"
+            }
+        }
+
+    }
+
+    private fun registerLauncher(){
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK){
+                storage_permission_text.text = "Read External Storage: True"
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if (result){
+                //permission granted
+                storage_permission_text.text = "Read External Storage: True"
+            }else{
+                //permission denied
+                Toast.makeText(applicationContext, "Permisson needed!", Toast.LENGTH_LONG).show()
+            }
         }
 
     }
